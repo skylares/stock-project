2377940a5cd0f093f7ae12409110f5834442757f
Skylar Kesselring <skylarkesselring@gmail.com>
2022-03-03 18:54:03 -0500

    Fix chart failing to display data

--- client/chart.js
+++ client/chart.js
@@ -1,37 +1,48 @@
-const ctx = document.getElementById('chart').getContext('2d');
-const chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+
+chartIt();
+async function chartIt() {
+
+    const xaxis = [];
+    const yaxis = [];
+    const response = await fetch('search/' + location.search.substring(1));
+    await response.json().then(result => {
+
+
+        result.forEach((stock, i) => {
+            let label = stock.time.substring(5, 10);
+            let openPrice = parseInt(stock.open);
+            yaxis.push(openPrice);
+            xaxis.push(label);
+        });
+
+    });
+
+    const down = (ctx, value) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+    const ctx = document.getElementById('chart').getContext('2d');
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xaxis, //xaxis
+            datasets: [{
+                label: 'daily closing price',
+                data: yaxis, //yaxis
+                borderColor: 'rgba(75, 192, 192, 1)',
+                segment: {
+                    borderColor: ctx => down(ctx, 'rgb(192,75,75)'),
                 }
             }]
+        },
+        options: {
+            fill: false,
+            interaction: {
+              intersect: false
+            },
+            radius: 0,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
-    }
-});
+    });
+} 
--- server.js
+++ server.js
@@ -1,29 +1,82 @@
-const Alpaca = require('@alpacahq/alpaca-trade-api');
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log("listening to port 3000"));
+app.use(express.static('client'));
 
+const mongoose = require('mongoose');
+const Stock = require("./Stock");
+mongoose.connect("mongodb://localhost/testdb");
+
+const Alpaca = require('@alpacahq/alpaca-trade-api');
 const alpaca = new Alpaca({
   keyId: process.env.API_KEY,
   secretKey: process.env.SECRET_API_KEY,
   paper: true,
 });
 
-async function spyData() {
+app.get('/search/:ticker', async (request, response) => {
 
   const data = await alpaca.getBarsV2(
-    'SPY',
+    request.params.ticker,
     {
       start: "2021-01-01",
       end: "2022-01-01",
       timeframe: "1Day",
       adjustment: "all",
     },
-    alpaca.configuration
+    alpaca.configuration,
   );
-  const bars = [];
-
+  const dailyPrice = [];
   for await (const b of data) {
-
-    console.log(b);
+    dailyPrice.push(b);
   }
-}
 
-spyData();
+  await dailyPrice.forEach(n => {
+
+    const stock = Stock.create({
+
+      ticker: request.params.ticker,
+      time: n.Timestamp,
+      open: n.OpenPrice,
+      close: n.ClosePrice,
+      low: n.LowPrice,
+      high: n.HighPrice,
+      volume: n.Volume,
+      vwap: n.VWAP,
+    });
+  });
+
+  response.json(await Stock.find({ ticker: "AMD"}));
+  //response.json(dailyPrice);
+});
+
+// dbRun();
+// async function dbRun() {
+//   try{
+//     const stock = await Stock.create({
+
+//       ticker: dumbData.ticker,
+//       time: dumbData.time,
+//       open: dumbData.open,
+//       close: dumbData.close,
+//       low: dumbData.low,
+//       high: dumbData.high,
+//       volume: dumbData.volume,
+//       vwap: dumbData.vwap,
+//     });
+//     console.log(stock);
+//   }
+//   catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// async function dbRun() {
+//   try{
+//     const stock = await Stock.find({ ticker: "AMD"});
+//     console.log(stock);
+//   }
+//   catch (error) {
+//     console.log(error.message);
+//   }
+// }
